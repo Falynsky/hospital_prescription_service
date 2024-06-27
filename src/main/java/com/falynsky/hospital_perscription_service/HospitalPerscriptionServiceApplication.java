958258c0bf209d13a749992b403f2849cff91f0b
Kamil Faliński <kamil.falinski@gmail.com>
2024-06-28 01:57:36 +0200

    init commit

---
+++ src/main/java/com/falynsky/hospital_perscription_service/HospitalPerscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.falynsky.hospital_perscription_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPerscriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalPerscriptionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/falynsky/hospital_perscription_service/HospitalPerscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falynsky.hospital_perscription_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalPerscriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
